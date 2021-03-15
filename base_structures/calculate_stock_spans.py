947190c85642f5840b77a1539f84e851e8987c28
Evgeniy Chumachenko <epchumachenko@avito.ru>
2021-03-15 12:57:05 +0300

    calculate stock spans

--- base_structures/calculate_stock_spans.py
+++ base_structures/calculate_stock_spans.py
@@ -1,11 +1,19 @@
-def calculate_stock_spans(prices: list) -> list:
-    return [1 for _ in prices]
+def validate_pushed_popped(pushed: list, popped: list) -> bool:
+    stack_in = []
+    stack_out = []
+    for i in range(len(pushed)):
+        stack_in.append(pushed[i])
+        while len(stack_in) > 0 and stack_in[len(stack_in) - 1] == popped[len(stack_out)]:
+            el = stack_in.pop()
+            stack_out.append(el)
+    return stack_out == popped
 
 
 def solution():
-    prices = list(map(int, input().split()))
-    spans = calculate_stock_spans(prices)
-    print(' '.join(map(str, spans)))
+    pushed = list(map(int, input().split()))
+    popped = list(map(int, input().split()))
+    result = validate_pushed_popped(pushed, popped)
+    print(result)
 
 
 solution()
